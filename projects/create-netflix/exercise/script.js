29380522e5dd8afa6f93e29f2debdf5448c0e414
Milena K <62288662+xMelaa@users.noreply.github.com>
2022-12-16 22:58:37 +0000

    'netflix', ending the course

--- projects/create-netflix/exercise/script.js
+++ projects/create-netflix/exercise/script.js
@@ -44,75 +44,117 @@ window.onload = () => {
 // ** Helper function that makes dynamic API calls **
 function fetchMovies(url, dom_element, path_type) {
   // Use Fetch with the url passed down 
-
+  fetch(url).
+    then(response => {
+      if (response.ok)
+        return response.json();
+      else
+        throw new Error('something went wrong');
+    }).then(data => {
+      showMovies(data, dom_element, path_type);
+    }).catch(error => {
+      console.log(error);
+    })
   // Within Fetch get the response and call showMovies() with the data , dom_element, and path type
 }
 
+
+
 //  ** Function that displays the movies to the DOM **
 showMovies = (movies, dom_element, path_type) => {
-  
+
   // Create a variable that grabs id or class
+  let moviesEl = document.querySelector(dom_element);
 
-
-  // Loop through object
-
-
+  // Loop through movies object
+  for (let movie of movies.results) {
+    let imageElement = document.createElement('img');
     // Within loop create an img element
-
-
     // Set attribute
-
+    imageElement.setAttribute('data-id', movie.id);
 
     // Set source
+    imageElement.src = `https://image.tmdb.org/t/p/original${movie[path_type]}`;
 
-
-    // Add event listener to handleMovieSelection() onClick
-
-  
+    imageElement.addEventListener('click', e => {
+      handleMovieSelection(e);
+    });
+    
     // Append the imageElement to the dom_element selected
-
-  }
+    moviesEl.appendChild(imageElement);
+  }  
 }
 
 // ** Function that fetches Netflix Originals **
 function getOriginals() {
+  let url = 'https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213';
 
+  fetchMovies(url, '.original__movies', 'poster_path');
 }
 // ** Function that fetches Trending Movies **
 function getTrendingNow() {
+  let url = 'https://api.themoviedb.org/3/trending/movie/week?api_key=19f84e11932abbc79e6d83f82d6d1045';
 
+  fetchMovies(url, '#trending', 'backdrop_path');
 }
 // ** Function that fetches Top Rated Movies **
 function getTopRated() {
+  let url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1';
 
+  fetchMovies(url, '#top_rated', 'backdrop_path');
 }
+
 
 // ** BONUS **
 
 // ** Fetches URL provided and returns response.json()
 async function getMovieTrailer(id) {
-  //URL: `https://api.themoviedb.org/3/movie/${id}/videos?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US`
+  let url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US`;
 
+  return await fetch(url)
+    .then (response => {
+      if(response.ok) 
+        return response.json();
+      else
+        throw new Error('something went wrong');
+    });
 }
 
 // ** Function that adds movie data to the DOM
 const setTrailer = trailers => {
   // Set up iframe variable to hold id of the movieTrailer Element
-  const iframe
+  const iframe = document.getElementById('movieTrailer');
   // Set up variable to select .movieNotFound element
-  const movieNotFound
+  const movieNotFound = document.querySelector('.movieNotFound');
 
   // If there is a trailer add the src for it
   if (trailers.length > 0) {
     // add d-none class to movieNotFound and remove it from iframe
-
+    movieNotFound.classList.add('d-none');
+    iframe.classList.remove('d-none');
     // add youtube link with trailers key to iframe.src
+    iframe.src = `https://www.youtube.com/embed/${trailers[0].key}`;
   } else {
     // Else remove d-none class to movieNotfound and ADD it to iframe
-
+    iframe.classList.add('d-none');
+    movieNotFound.classList.remove('d-none');
   }
 }
 
+const handleMovieSelection = e => {
+  const id = e.target.getAttribute('data-id');
+  const iframe = document.getElementById('movieTrailer'); 
 
+  getMovieTrailer(id).then(data => {
+    const results = data.results;
+    const youtubeTrailers = results.filter(result => {
+      if(result.site == 'YouTube' && result.type == 'Trailer')
+        return true;
+      else
+        return false;
+    })
+    setTrailer(youtubeTrailers);
+  })
 
-
+  $('#trailerModal').modal('show');
+}
